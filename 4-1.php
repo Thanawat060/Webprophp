388199b8cb1a4f01b6b36613d0185ef4b4bbc4b3
Your Name <you@example.com>
2021-01-13 11:10:04 +0700

    update

---
+++ 4-1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x =100;
+        $y ="200";
+        $z ='300';
+        $add =$x +$y;
+        $sub =$z -$x;
+        $mul =$y*$z;
+        $div =$mul/300;
+        $message ="Hello World";
+        echo $x;    echo"<br />";
+        echo $y."<br/>";
+        echo $z."<br/>";
+        echo $add."<br/>";
+        echo $sub."<br/>";
+        echo $mul."<br/>";
+        echo $div."<br/>";
+        echo $message."<br/>";
+        ?>
+</body>
+</html>
