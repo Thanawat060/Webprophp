d325cb0803eb7810f8621d785866351ecb13214e
Your Name <you@example.com>
2021-01-20 11:53:17 +0700

    update

--- result.php
+++ result.php
@@ -5,8 +5,12 @@
     $y = $_GET["numy"];
     $op = $_GET["text"];
     ?>
-
+Show Result <br>
+Num X = <?php echo  $x ?> <br>
+Num Y = <?php echo  $y ?> <br>
+Opeartor = <?php echo  $op ?> <br><br>
     <?php
+    
     if($op == "+"){
         echo" $x+$y = " ,$x+$y;
     }
@@ -20,5 +24,7 @@
         echo" $x/$y = " ,$x/$y;;
     }
     ?>
+    <form action ="formcal.php" method ="get">
+        <input type ="submit">
 </body>
 </html>
