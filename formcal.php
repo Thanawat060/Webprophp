d4fbd30547f7c99df8c0d3d86ed2f1e1039b906a
Your Name <you@example.com>
2021-01-29 23:54:18 +0700

    update

--- formcal.php
+++ formcal.php
@@ -1,8 +1,8 @@
 <html>
     <body>
     <form action="result.php" method="get">
-    Num X :<input type = "numx" name="numx"><br>
-    Num Y :<input type = "numy" name="numy"><br>
+    Num X! :<input type = "text" name="numx"><br>
+    Num Y! :<input type = "text" name="numy"><br>
     Opeartor :<input type = "text" name="text"><br>
     <input type = "submit">
 </body>
