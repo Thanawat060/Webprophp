f0448dd3b565bab70dadab128a34b7b058a13e3c
Your Name <you@example.com>
2021-02-16 21:03:18 +0700

    update

--- Exweek7/ex01.php
+++ Exweek7/ex01.php
@@ -5,24 +5,23 @@
         <title>Find Ascii</title>
     </head>
     <body>
-        <form method="GET"  >
+        <form method="POST"  >
             <center>
-            <h1>HW2-2 : Find Ascii</h1>
-            Your Text : <input type="text" name="mes" ><br><br>
+            <h1>HW2-1 : Find Character</h1>
+            Your Text : <input type="text" name="data" ><br><br>
             <input type="submit" name="submit" value="Submit">
             <input type="reset" value="Clear">
             <br><br>
         </form>
         <?php
-            if(isset($_GET["submit"])){
+            if(isset($_POST["submit"])){
                 echo "<h2>Count Char in Sentence</h2>";
-                $message = $_GET["mes"];
-                echo $message;
-                foreach(count_chars($message,1) as $key => $cookierun){ 
-                    if(($key > 64 && $key < 91) ||($key > 96 && $key < 123)){
-                        echo chr($key)." Ascii =".$cookierun."<br>";
+                $data = $_POST["data"];
+                foreach(count_chars($data,1) as $value => $noun){
+                    if($value == 32){
+                        echo "Space";
                     }
-                   
+                    echo chr($value) ," = ", $noun ,"<br>";
                 }
             }
         ?>
